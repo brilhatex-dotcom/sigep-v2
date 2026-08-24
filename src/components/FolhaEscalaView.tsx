12e3761078920ceb342939f9841f7acb5a371f12
brilhatex-dotcom <brilhatex@gmail.com>
2026-08-24 09:20:04 -0300

    Refactor CMT and SUBCMT FT rows in FolhaEscalaView

--- src/components/FolhaEscalaView.tsx
+++ src/components/FolhaEscalaView.tsx
@@ -338,8 +338,8 @@ export default function FolhaEscalaView({
                 {expedienteFeriado ? (
                   <>
                     <tr><td className="lbl">CMT</td><td className="val feriado" rowSpan={5}>{feriadoTexto}</td><td className="lbl">SUBCMT</td><td className="val feriado" rowSpan={5}>{feriadoTexto}</td></tr>
-                    <tr><td className="lbl">CMT FT</td><td className="lbl">SUBCMT FT</td></tr>
-                    <tr><td className="lbl">P4</td><td className="lbl"></td></tr>
+                    <tr><td className="lbl">CMT FT</td><td className="lbl">P4</td></tr>
+                    <tr><td className="lbl">SUBCMT FT</td><td className="lbl"></td></tr>
                     <tr><td className="lbl">P1</td><td className="lbl">RONDA ESCOLAR</td></tr>
                     <tr><td className="lbl">P3</td><td className="lbl">PATRULHA MARIA DA PENHA</td></tr>
                   </>
@@ -354,14 +354,12 @@ export default function FolhaEscalaView({
                     <tr>
                       <td className="lbl">CMT FT</td>
                       <td className="val val-c"><T html={exp.cmtFt} /></td>
-                      <td className="lbl">SUBCMT FT</td>
-                      <td className="val val-c"><T html={exp.subcmtFt} /></td>
+                      <td className="lbl">P4</td>
+                      <td className="val" rowSpan={2}><ListaTexto valor={exp.p4} center semPermuta /></td>
                     </tr>
                     <tr>
-                      <td className="lbl">P4</td>
-                      <td className="val"><ListaTexto valor={exp.p4} center semPermuta /></td>
-                      <td className="lbl"></td>
-                      <td className="val"></td>
+                      <td className="lbl">SUBCMT FT</td>
+                      <td className="val val-c"><T html={exp.subcmtFt} /></td>
                     </tr>
                     <tr>
                       <td className="lbl">P1</td>
