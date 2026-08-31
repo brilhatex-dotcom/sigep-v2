dc7b809a2f89e6635ec1601d691912ae6eabe9fe
brilhatex-dotcom <brilhatex@gmail.com>
2026-08-31 10:59:11 -0300

    Update EscalaClient.tsx

--- src/app/escalas/EscalaClient.tsx
+++ src/app/escalas/EscalaClient.tsx
@@ -47,6 +47,7 @@ type Expediente = {
   subcmt: string;
   cmtFt: string;
   subcmtFt: string;
+  adm: string;
   p4: Slot[];
   p1: Slot[];
   rondaEscolar: Slot[];
@@ -579,6 +580,7 @@ function expedientePadrao(): Expediente {
     subcmt: "Major QOEM Frans",
     cmtFt: "Major QOEM Paulo Roberto",
     subcmtFt: "Asp Of QOEM Macedo",
+    adm: "",
     p4: [
       s("Major QOE Herbert"),
       s("2º Sgt. PM nº 122/07- Júlio"),
@@ -2721,9 +2723,8 @@ export default function EscalaClient() {
                   <tr><td className="hd" colSpan={4}>EXPEDIENTE <Editable value={e.expediente.horario} onChange={(v) => editE((d) => { d.expediente.horario = v; })} /></td></tr>
                   {expedienteFeriado ? (
                     <>
-                      <tr><td className="lbl">CMT</td><td className="val feriado" rowSpan={5}>{feriadoTexto}</td><td className="lbl">SUBCMT</td><td className="val feriado" rowSpan={5}>{feriadoTexto}</td></tr>
-                      <tr><td className="lbl">CMT FT</td><td className="lbl">P4</td></tr>
-                      <tr><td className="lbl">SUBCMT FT</td><td className="lbl"></td></tr>
+                      <tr><td className="lbl">CMT</td><td className="val feriado" rowSpan={4}>{feriadoTexto}</td><td className="lbl">SUBCMT</td><td className="val feriado" rowSpan={4}>{feriadoTexto}</td></tr>
+                      <tr><td className="lbl">FT</td><td className="lbl">P4</td></tr>
                       <tr><td className="lbl">P1</td><td className="lbl">RONDA ESCOLAR</td></tr>
                       <tr><td className="lbl">P3</td><td className="lbl">PATRULHA MARIA DA PENHA</td></tr>
                     </>
@@ -2736,14 +2737,14 @@ export default function EscalaClient() {
                         <td className="val val-c"><SlotInline efetivo={efetivo} semPermuta slot={s2(e.expediente.subcmt)} onChange={(ns) => editE((d) => { d.expediente.subcmt = ns.titular; })} /></td>
                       </tr>
                       <tr>
-                        <td className="lbl">CMT FT</td>
-                        <td className="val val-c"><SlotInline efetivo={efetivo} semPermuta slot={s2(e.expediente.cmtFt)} onChange={(ns) => editE((d) => { d.expediente.cmtFt = ns.titular; })} /></td>
-                        <td className="lbl" rowSpan={2}>P4</td>
-                        <td className="val" rowSpan={2}><SlotList efetivo={efetivo} semPermuta center slots={e.expediente.p4} ordenar onChange={(arr) => editE((d) => { d.expediente.p4 = arr; })} /></td>
-                      </tr>
-                      <tr>
-                        <td className="lbl">SUBCMT FT</td>
-                        <td className="val val-c"><SlotInline efetivo={efetivo} semPermuta slot={s2(e.expediente.subcmtFt)} onChange={(ns) => editE((d) => { d.expediente.subcmtFt = ns.titular; })} /></td>
+                        <td className="lbl">FT</td>
+                        <td className="val val-c">
+                          <div><b>CMT</b> <SlotInline efetivo={efetivo} semPermuta slot={s2(e.expediente.cmtFt)} onChange={(ns) => editE((d) => { d.expediente.cmtFt = ns.titular; })} /></div>
+                          <div><b>SUBCMT</b> <SlotInline efetivo={efetivo} semPermuta slot={s2(e.expediente.subcmtFt)} onChange={(ns) => editE((d) => { d.expediente.subcmtFt = ns.titular; })} /></div>
+                          <div><b>ADM</b> <SlotInline efetivo={efetivo} semPermuta slot={s2(e.expediente.adm)} onChange={(ns) => editE((d) => { d.expediente.adm = ns.titular; })} /></div>
+                        </td>
+                        <td className="lbl">P4</td>
+                        <td className="val"><SlotList efetivo={efetivo} semPermuta center slots={e.expediente.p4} ordenar onChange={(arr) => editE((d) => { d.expediente.p4 = arr; })} /></td>
                       </tr>
                       <tr>
                         <td className="lbl">P1</td>
